6a6fa5d627b5287ee8a3ca7a666037e5e76bad7b
KJH <jhmn23@naver.com>
2021-10-05 15:14:24 +0900

    1005

--- 03_단일행함수.sql
+++ 03_단일행함수.sql
@@ -51,3 +51,124 @@ SELECT employee_id 짝수번째, last_name
 FROM employees
 WHERE MOD(employee_id, 2) = 0 -- 짝수
 ORDER BY 짝수번째;
+
+
+-- 날짜형 함수 , sysdate는 현재 날짜를 의미
+
+-- 일은 시간이 12시 이상이면 반올림
+SELECT SYSDATE 현재날짜, ROUND(sysdate, 'DD') 일, ROUND(sysdate, 'MM') 월, ROUND(sysdate, 'YY') 년도
+        , ROUND( MONTHS_BETWEEN('2021/10/5', '2021/06/10') , 1 ) 월차이
+FROM DUAL;
+
+SELECT hire_date FROM employees;
+
+-- 날짜에 더하기 빼기를 하면 => 실제 날짜가 더하거나 빼진다.
+SELECT sysdate 오늘날짜, sysdate + 1 내일 , sysdate -1 어제
+FROM DUAL;
+
+-- 예제 1)
+SELECT sysdate, hire_date, MONTHS_BETWEEN(sysdate, hire_date) 적용결과
+FROM employees
+WHERE department_id = '100';
+
+-- 예제 2)
+SELECT hire_date, ADD_MONTHS(hire_date, 3) 더하기_3개월, ADD_MONTHS(hire_date, -3) 빼기_3개월
+FROM employees
+WHERE employee_id BETWEEN 100 AND 106;
+
+
+-- 변환형 함수
+-- 숫자 => 문자로 변환
+
+SELECT 
+    TO_CHAR( 12345678 , '999,999,999') 콤마형식,
+    TO_CHAR( 12345678.678 , '999,999,999.99') 콤마소수점형식,
+    TO_CHAR( 12345678 , '$999,999,999') 달러형식,
+    TO_CHAR( 12345678 , 'L999,999,999') 로컬통화
+FROM DUAL;
+
+-- 날짜 => 문자로 변환
+
+SELECT
+    TO_CHAR( sysdate , 'YYYY-MM-DD') 날짜표시,
+    TO_CHAR( sysdate , 'YYYY-MM-DD HH24:MI:SS') 시간표시
+FROM DUAL;
+
+-- 예제 1)
+SELECT employee_id, TO_CHAR( hire_date, 'MM/YY') 입사월
+FROM employees
+WHERE department_id = 100;
+
+-- 예제 2)
+SELECT last_name 이름, TO_CHAR( salary, '$99,999.00') 월급 
+FROM employees
+WHERE salary > 10000
+ORDER BY salary DESC;
+
+-- 문자열 => 날짜 변환
+SELECT TO_DATE('2011/01/01', 'YYYY/MM/DD')
+FROM DUAL;
+
+-- 문자열 => 숫자 변환
+SELECT TO_NUMBER('0123')+100
+FROM DUAL;
+
+
+-- 널값 관련 함수 (null)
+
+-- NVL(값 , 0) => 만약 값이 null이면 0으로 변환
+SELECT last_name 이름, NVL(manager_id, 0) 매니저
+FROM employees WHERE last_name = 'King';
+
+-- NVL2(값 , 첫번째 , 두번째) => 만약 값이 null값이 아니면 첫번째 , null값이면 두번째
+SELECT last_name 이름, NVL2(manager_id, 1, 0) 매니저
+FROM employees WHERE last_name = 'King';
+
+-- 예제 1)
+SELECT last_name 이름, salary 월급, NVL(commission_pct, 0) 커미션, salary*12 + salary*12*NVL(commission_pct, 0) 연봉
+FROM employees ORDER BY 연봉 DESC;
+
+-- 예제 2)
+SELECT 
+    last_name 이름, salary 월급, NVL(commission_pct, 0) 커미션, salary*12 + salary*12*NVL(commission_pct, 0) 연봉,
+    NVL2(commission_pct, '월급+보너스' , '월급만') 연봉계산방법 
+FROM employees ORDER BY 연봉 DESC;
+
+
+-- decode 함수 DECODE(a, b, c) a의 값이 b면 c로 출력
+SELECT last_name 이름, job_id, salary,
+    DECODE(job_id, 'IT_PORG', salary * 1.10,
+                   'ST_CLERK', salary * 1.15,
+                   'SA_REP',  salary * 1.20,
+                              salary) "수정월급"
+FROM employees;
+
+-- 예제
+SELECT last_name 이름, job_id 직무, salary 월급,
+    DECODE(TRUNC(salary/2000), 0, 0,
+                               1, 0.09,
+                               2, 0.2,
+                               3, 0.3,
+                               4, 0.4,
+                               5, 0.42,
+                               6, 0.44,
+                                  0.45) 세율
+FROM employees;
+
+-- CASE 함수 비교가능
+SELECT last_name 이름, job_id 직무, salary 월급,
+    CASE WHEN salary<5000    THEN 'Low'
+         WHEN salary<10000   THEN 'Medium'
+         WHEN salary<20000   THEN 'Good'
+         ELSE                     'Excellent'
+    END "급여 수준"
+FROM employees;
+
+-- 예제
+SELECT employee_id, first_name, last_name, salary,
+    CASE WHEN salary>=9000                   THEN '상위급여'
+         WHEN salary BETWEEN 6000 AND 8999   THEN '중위급여'
+         ELSE                                     '하위급여'
+    END "급여 등급"
+FROM employees
+WHERE job_id = 'IT_PROG';
